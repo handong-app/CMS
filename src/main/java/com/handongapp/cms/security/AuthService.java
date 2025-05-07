ac563e6512ef285161c963d5b69e20e5632517d0
7SH7 <kimseunghwan7777@gmail.com>
2025-05-07 16:24:34 +0900

    ✨ Feat: implement google login and spring security
    구글 로그인과 spring 시큐리티 필터 적용 완성

---
+++ src/main/java/com/handongapp/cms/security/AuthService.java
@@ -0,0 +1,24 @@
+package com.handongapp.cms.security;
+
+import io.jsonwebtoken.Claims;
+
+import java.util.Map;
+
+public interface AuthService {
+
+    String createAccessToken(Map<String, Object> claims, String subject);
+
+    String createRefreshToken(String subject);
+
+    boolean validateAccessToken(String token);
+
+    boolean validateRefreshToken(String token);
+
+    Claims getAccessClaims(String token);
+
+    Claims getRefreshClaims(String token);
+
+    String getSubjectFromAccess(String token);
+
+    String getSubjectFromRefresh(String token);
+}
