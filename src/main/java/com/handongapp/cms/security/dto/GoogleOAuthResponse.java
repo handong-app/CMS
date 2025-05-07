b2f6a2711be3d0ba14edc7d075de0b0384fbd243
7SH7 <kimseunghwan7777@gmail.com>
2025-05-07 17:14:46 +0900

    ♻️ Refactor: change access modifiers and adjust setter methods for better encapsulation
    접근 제어자, @Setter 제거

--- src/main/java/com/handongapp/cms/security/dto/GoogleOAuthResponse.java
+++ src/main/java/com/handongapp/cms/security/dto/GoogleOAuthResponse.java
@@ -4,15 +4,14 @@ import com.handongapp.cms.domain.Tbuser;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
+
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
-@Setter
 public class GoogleOAuthResponse {
-    String accessToken;
-    String refreshToken;
-    long expiresIn;
-    Tbuser member;
+    private String accessToken;
+    private String refreshToken;
+    private long expiresIn;
+    private Tbuser member;
 }
