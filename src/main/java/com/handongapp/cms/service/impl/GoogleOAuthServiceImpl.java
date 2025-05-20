5ebeedc8a3ed4d5910cbe6cce35d699d764c6210
7SH7 <kimseunghwan7777@gmail.com>
2025-05-21 04:52:03 +0900

    ♻️ Refactor: make function that build claims
    claim 만드는 기능 중복 -> 함수로 빼어냄.

--- src/main/java/com/handongapp/cms/service/impl/GoogleOAuthServiceImpl.java
+++ src/main/java/com/handongapp/cms/service/impl/GoogleOAuthServiceImpl.java
@@ -66,13 +66,7 @@ public class GoogleOAuthServiceImpl implements GoogleOAuthService {
         Tbuser tbuser = tbuserService.processGoogleUser(userInfo);
 
         // 4. JWT claims 생성
-        Map<String, Object> claims = new HashMap<>();
-        claims.put("email", Optional.ofNullable(tbuser.getEmail()).orElse(""));
-        claims.put("name", Optional.ofNullable(tbuser.getName()).orElse(""));
-        claims.put("role", Optional.ofNullable(tbuser.getRole())
-                .map(Enum::name) // return string like "USER"
-                .orElse("USER")); // 기본값도 보장(로그인하면, 기본값은 user)
-        claims.put("student", Optional.ofNullable(tbuser.getStudentId()).orElse(""));
+        Map<String, Object> claims = buildClaims(tbuser);
 
         String access = authService.createAccessToken(claims, tbuser.getEmail());
         String refresh = authService.createRefreshToken(tbuser.getEmail());
@@ -97,16 +91,21 @@ public class GoogleOAuthServiceImpl implements GoogleOAuthService {
         Optional<Tbuser> userOpt = tbuserRepository.findByEmail(email);
         if (userOpt.isPresent()) {
             Tbuser tbuser = userOpt.get();
-            Map<String, Object> claims = new HashMap<>();
-            claims.put("email", tbuser.getEmail());
-            claims.put("name", tbuser.getName());
-            claims.put("role", Optional.ofNullable(tbuser.getRole())
-                    .map(Enum::name)
-                    .orElse("USER"));
-            claims.put("student", tbuser.getStudentId());
+            Map<String, Object> claims = buildClaims(tbuser);
             return authService.createAccessToken(claims, email);
         }
         return authService.createAccessToken(Map.of(), email);
-
     }
+
+    private Map<String, Object> buildClaims(Tbuser tbuser) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("email", Optional.ofNullable(tbuser.getEmail()).orElse(""));
+        claims.put("name", Optional.ofNullable(tbuser.getName()).orElse(""));
+        claims.put("role", Optional.ofNullable(tbuser.getRole())
+                .map(Enum::name)
+                .orElse("USER"));
+        claims.put("student", Optional.ofNullable(tbuser.getStudentId()).orElse(""));
+        return claims;
+    }
+
 }
