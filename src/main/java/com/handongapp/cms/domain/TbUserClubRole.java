9d14f140303afee824314455193d2a43209c58c2
7SH7 <kimseunghwan7777@gmail.com>
2025-05-26 04:20:26 +0900

    🐛 Fix: solve the login problem
    로그인이 안 되던 문제 해결

--- src/main/java/com/handongapp/cms/domain/TbUserClubRole.java
+++ src/main/java/com/handongapp/cms/domain/TbUserClubRole.java
@@ -20,13 +20,13 @@ public class TbUserClubRole extends AuditingFields {
     @Column(name = "user_id", columnDefinition = "CHAR(32)", nullable = false) @Setter
     private String userId;
 
-    @Column(name = "club_id", columnDefinition = "CHAR(32)", nullable = false) @Setter
+    @Column(name = "club_id", columnDefinition = "CHAR(32)", nullable = true) @Setter
     private String clubId;
 
     @Column(name = "role_id", columnDefinition = "CHAR(32)", nullable = false) @Setter
     private String roleId;
 
-    @Column(name = "generation", nullable = false) @Setter
+    @Column(name = "generation", nullable = true) @Setter
     private Integer generation;
 
     public TbUserClubRole(String userId, String clubId, String roleId, Integer generation) {
--- src/main/java/com/handongapp/cms/repository/TbUserRepository.java
+++ src/main/java/com/handongapp/cms/repository/TbUserRepository.java
@@ -11,4 +11,5 @@ public interface TbUserRepository extends JpaRepository<TbUser, Long> {
     Optional<TbUser> findByEmail(String email);
 
     Optional<TbUser> findById(String Id);
+    Optional<TbUser> findByGoogleSub(String GoogleSub);
 }
