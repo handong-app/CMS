bf23b3662359b6f992c7365c7ecee5c2211fe7eb
7SH7 <kimseunghwan7777@gmail.com>
2025-05-26 04:34:00 +0900

    ♻️ Refactor: modify tbclub entity
    TbClub entity 변수 속성 변환 (@URL, @NotBlanck 추가)

--- src/main/java/com/handongapp/cms/domain/TbClub.java
+++ src/main/java/com/handongapp/cms/domain/TbClub.java
@@ -4,9 +4,11 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Index;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.URL;
 
 @Getter
 @Entity
@@ -20,11 +22,11 @@ import lombok.Setter;
 )
 public class TbClub extends AuditingFields {
 
-    @Column(length = 120, nullable = false) @Setter private String name;
+    @Column(length = 120, nullable = false) @NotBlank @Setter private String name;
 
-    @Column(length = 100, nullable = false) @Setter private String slug;
+    @Column(length = 100, nullable = false) @NotBlank @Setter private String slug;
 
-    @Column(columnDefinition = "TEXT") @Setter private String bannerUrl;
+    @Column(columnDefinition = "TEXT") @URL @Setter private String bannerUrl;
 
     @Column(columnDefinition = "TEXT") @Setter  private String description;
 }
