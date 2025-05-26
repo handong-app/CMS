65fee50069ed5ec9349ad13f902a6014d31708f7
7SH7 <kimseunghwan7777@gmail.com>
2025-05-27 07:07:05 +0900

    ♻️ Refactor: clean not using line

--- src/main/java/com/handongapp/cms/controller/GoogleLoginController.java
+++ src/main/java/com/handongapp/cms/controller/GoogleLoginController.java
@@ -10,7 +10,6 @@ import com.handongapp.cms.service.TbUserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.Map;
 
