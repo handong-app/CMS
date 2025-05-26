aaab76e91fab7cbcec2ee4c250a9bccd589eff02
anchovy1st <anchovy1st@gmail.com>
2025-05-27 00:51:11 +0900

    :sparkles: feat: add nodeRepository method

--- src/main/java/com/handongapp/cms/repository/TbNodeRepository.java
+++ src/main/java/com/handongapp/cms/repository/TbNodeRepository.java
@@ -3,7 +3,9 @@ package com.handongapp.cms.repository;
 import com.handongapp.cms.domain.TbNode;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface TbNodeRepository extends JpaRepository<TbNode, String> {
+    List<TbNode> findByNodeGroupIdAndDeleted(String nodeGroupId, String deleted);
 }
