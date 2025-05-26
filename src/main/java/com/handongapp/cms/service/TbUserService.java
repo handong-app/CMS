4b573cf1e69747e498b1284685c211c1accadfd0
7SH7 <kimseunghwan7777@gmail.com>
2025-05-27 01:48:29 +0900

    ♻️ Refactor: remove line

--- src/main/java/com/handongapp/cms/service/TbUserService.java
+++ src/main/java/com/handongapp/cms/service/TbUserService.java
@@ -7,6 +7,5 @@ import com.handongapp.cms.security.dto.GoogleUserInfoResponse;
 public interface TbUserService {
     TbUser saveOrUpdateUser(String userId, String email, String name);
     TbUser processGoogleUser(GoogleUserInfoResponse googleUserInfoResponse);
-
     void updateUserProfile(TbUserDto.UpdateUserProfileReqDto reqDto);
 }
