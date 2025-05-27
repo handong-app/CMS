095b0ee549cd7c9eef9ea8df9f9ff4e6e8176ff5
junglesub <junglesubmarine@gmail.com>
2025-05-27 21:37:06 +0900

    ♻️ refactor: api base code

--- src/main/front/src/store/useProductStore.ts
+++ src/main/front/src/store/useProductStore.ts
@@ -1,8 +1,9 @@
 import { create } from "zustand";
+import { ProductItemProps } from "../types/product.types";
 
 interface ProductState {
-  products: any[]; // 실제 프로젝트에서는 any 대신 구체적인 타입을 사용하세요.
-  addProduct: (product: any) => void; // 실제 프로젝트에서는 any 대신 구체적인 타입을 사용하세요.
+  products: ProductItemProps[];
+  addProduct: (product: ProductItemProps) => void;
 }
 
 const useProductStore = create<ProductState>((set) => ({
