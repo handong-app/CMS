5ee0ea7298f1ba56d2c790e1be7563526d96417f
callendev20 <callendev20@gmail.com>
2025-05-29 03:06:44 +0900

    🔧  be/feat: add RabbitMQ configuration with JSON message converter and transcode queue

---
+++ src/main/java/com/handongapp/cms/config/RabbitMQConfig.java
@@ -0,0 +1,35 @@
+package com.handongapp.cms.config;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Value("${rabbitmq.queue.transcode-request}")
+    private String transcodeRequestQueue;
+
+    @Bean
+    public Queue transcodeRequestQueue() {
+        return new Queue(transcodeRequestQueue, true);
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public AmqpTemplate amqpTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(jsonMessageConverter());
+        return template;
+    }
+}
