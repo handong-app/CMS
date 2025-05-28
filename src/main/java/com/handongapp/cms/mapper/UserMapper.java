dd9db27e5c26a88c580a5ddb9028dd5c7d807906
7SH7 <kimseunghwan7777@gmail.com>
2025-05-29 00:39:20 +0900

    ♻️ BE/Refactor: remove Tb tb keyword

--- src/main/java/com/handongapp/cms/mapper/UserMapper.java
+++ src/main/java/com/handongapp/cms/mapper/UserMapper.java
@@ -4,6 +4,6 @@ import com.handongapp.cms.dto.v1.UserDto;
 
 import java.util.List;
 
-public interface TbUserMapper {
+public interface UserMapper {
     List<UserDto.LastProgramResDto> findLastNodeGroupByCourseForUser(String userId);
 }
