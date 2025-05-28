368be3b9c13bc5f66e85a008c19141393fbdb0da
callendev20 <callendev20@gmail.com>
2025-05-29 02:17:54 +0900

    :sparkles: be/feat: implement PresignedUrlService - generateUploadUrl, generateDownloadUrl

---
+++ src/main/java/com/handongapp/cms/service/impl/PresignedUrlServiceImpl.java
@@ -0,0 +1,75 @@
+package com.handongapp.cms.service.impl;
+
+import com.handongapp.cms.service.PresignedUrlService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Configuration;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest;
+
+import java.net.URI;
+import java.net.URL;
+import java.time.Duration;
+
+@Service
+public class PresignedUrlServiceImpl implements PresignedUrlService {
+
+    private final S3Presigner presigner;
+    private final String bucket;
+
+    public PresignedUrlServiceImpl(
+            @Value("${cloud.aws.s3.bucket}") String bucket,
+            @Value("${cloud.aws.credentials.access-key}") String accessKey,
+            @Value("${cloud.aws.credentials.secret-key}") String secretKey,
+            @Value("${cloud.aws.endpoint}") String endpoint
+    ) {
+        this.bucket = bucket;
+
+        this.presigner = S3Presigner.builder()
+                .credentialsProvider(StaticCredentialsProvider.create(
+                        AwsBasicCredentials.create(accessKey, secretKey)))
+                .endpointOverride(URI.create(endpoint))
+                .region(Region.US_EAST_1)
+                .serviceConfiguration(S3Configuration.builder()
+                        .pathStyleAccessEnabled(true)
+                        .build())
+                .build();
+    }
+
+    @Override
+    public URL generateUploadUrl(String filename, String contentType) {
+        PutObjectRequest objectRequest = PutObjectRequest.builder()
+                .bucket(bucket)
+                .key(filename)
+                .contentType(contentType)
+                .build();
+
+        PutObjectPresignRequest presignRequest = PutObjectPresignRequest.builder()
+                .signatureDuration(Duration.ofMinutes(10))
+                .putObjectRequest(objectRequest)
+                .build();
+
+        return presigner.presignPutObject(presignRequest).url();
+    }
+
+    @Override
+    public URL generateDownloadUrl(String key) {
+        GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                .bucket(bucket)
+                .key(key)
+                .build();
+
+        GetObjectPresignRequest presignRequest = GetObjectPresignRequest.builder()
+                .signatureDuration(Duration.ofMinutes(10))
+                .getObjectRequest(getObjectRequest)
+                .build();
+
+        return presigner.presignGetObject(presignRequest).url();
+    }
+}
