7a894d183a8b1c947887d1e8994cdbe3f9c30a80
callendev20 <callendev20@gmail.com>
2025-05-29 21:26:04 +0900

    :white_check_mark: be/feat: add PresignedUrlCreationException

---
+++ src/main/java/com/handongapp/cms/exception/file/PresignedUrlCreationException.java
@@ -0,0 +1,20 @@
+package com.handongapp.cms.exception.file;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * Presigned URL 생성 실패 시 발생하는 예외
+ * HttpStatus INTERNAL_SERVER_ERROR
+ */
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+public class PresignedUrlCreationException extends RuntimeException {
+
+    public PresignedUrlCreationException(String message) {
+        super(message);
+    }
+
+    public PresignedUrlCreationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
