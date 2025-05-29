6f340b22564ee156736a7e4bb3ef9d2e87e4e443
Twin_Whales <111281489+TwinWhales@users.noreply.github.com>
2025-05-30 03:55:17 +0900

    ✨ FE/Feat: set router
    구현한 페이지 라우터로 연결 구현

--- src/main/front/src/App.tsx
+++ src/main/front/src/App.tsx
@@ -1,34 +1,20 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import React from "react";
+import { Outlet } from "react-router-dom";
+import { Box } from "@mui/material";
+import MyAppBar from "./components/common/MyAppBar"; // 경로는 실제 위치에 맞게 조정
+import useAuthStore from "./store/authStore";
 
-function App() {
-  const [count, setCount] = useState(0);
+const App = () => {
+  const user = useAuthStore((state) => state.user);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <MyAppBar user={user} />
+      <Box component="main" mt={2}>
+        <Outlet />
+      </Box>
     </>
   );
-}
+};
 
 export default App;
