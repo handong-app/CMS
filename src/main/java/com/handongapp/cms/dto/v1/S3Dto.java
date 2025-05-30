eaa9472e43996a71a2b7140f98f9c5ead8538d87
callendev20 <callendev20@gmail.com>
2025-05-30 18:52:19 +0900

    🦺 be/feat: add validation annotations for file upload request fields
          - Add @NotBlank and @Pattern to ensure filename and contentType are valid
          - Improve security and data integrity in upload API

--- src/main/java/com/handongapp/cms/dto/v1/S3Dto.java
+++ src/main/java/com/handongapp/cms/dto/v1/S3Dto.java
@@ -1,6 +1,8 @@
 package com.handongapp.cms.dto.v1;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
 public class S3Dto {
@@ -13,7 +15,13 @@ public class S3Dto {
     @AllArgsConstructor
     @NoArgsConstructor
     public static class UploadUrlRequest {
+        @NotBlank(message = "파일명은 필수입니다")
         private String filename;
+        @NotBlank(message = "콘텐츠 타입은 필수입니다")
+        @Pattern(
+                regexp = "^[a-zA-Z0-9][a-zA-Z0-9\\!\\*'\\(\\)\\;\\:\\@\\&\\=\\+\\$\\,\\/\\?\\%\\#\\[\\]\\-\\_\\.\\~]*$",
+                message = "유효하지 않은 콘텐츠 타입입니다"
+        )
         private String contentType;
     }
 
