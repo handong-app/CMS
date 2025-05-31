558db895a9f643ca83a4aab23feb03a1dc92f162
callendev20 <callendev20@gmail.com>
2025-06-01 00:58:36 +0900

    :sparkles: be/feat: implement NodeMapper.findNodeTypeById

--- src/main/java/com/handongapp/cms/mapper/NodeMapper.java
+++ src/main/java/com/handongapp/cms/mapper/NodeMapper.java
@@ -1,7 +1,16 @@
 package com.handongapp.cms.mapper;
 
+import com.handongapp.cms.domain.TbNode;
 import org.apache.ibatis.annotations.Param;
 
 public interface NodeMapper {
+    /**
+     * 노드 ID로 클럽ID를 조회
+     */
     String findClubIdByNodeId(@Param("nodeId") String nodeId);
+    /**
+     * 노드 ID로 타입을 조회
+     */
+    TbNode.NodeType findNodeTypeById(@Param("nodeId") String nodeId);
+
 }
