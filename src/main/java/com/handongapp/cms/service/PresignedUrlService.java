4758dfa97f9a92a6d909bc0b5b22c6772d12713f
callendev20 <callendev20@gmail.com>
2025-06-01 01:10:26 +0900

    :white_check_mark: be/feat: add generateNodeFileUploadUrl definition and refactor generateUploadUrl definition - PresignedUrlService

--- src/main/java/com/handongapp/cms/service/PresignedUrlService.java
+++ src/main/java/com/handongapp/cms/service/PresignedUrlService.java
@@ -1,9 +1,13 @@
 package com.handongapp.cms.service;
 
+import com.handongapp.cms.dto.v1.S3Dto;
+
 import java.net.URL;
 
 
 public interface PresignedUrlService {
-    URL generateUploadUrl(String filename, String contentType);
+    S3Dto.UploadUrlResponse generateUploadUrl(String path, String id, String originalFilename, String extension, String contentType);
     URL generateDownloadUrl(String key);
+
+    S3Dto.UploadUrlResponse generateNodeFileUploadUrl(S3Dto.NodeFileUploadUrlRequest request, String userId);
 }
