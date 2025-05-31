24303e873c182d1232cd6afc04ae9bb957f02722
callendev20 <callendev20@gmail.com>
2025-05-31 18:48:50 +0900

    :recycle: be/fix: revise id type Long to String - ClubRoleRepository

--- src/main/java/com/handongapp/cms/repository/ClubRoleRepository.java
+++ src/main/java/com/handongapp/cms/repository/ClubRoleRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ClubRoleRepository extends JpaRepository<TbClubRole, Long> {
+public interface ClubRoleRepository extends JpaRepository<TbClubRole, String> {
 }
