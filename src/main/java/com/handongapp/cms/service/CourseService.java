6cc25e87ac416824b9e4d08242c3e413876ffab2
anchovy1st <anchovy1st@gmail.com>
2025-05-31 14:41:04 +0900

    ♻️ Refactor: resolving code rabbit's RC

--- src/main/java/com/handongapp/cms/service/CourseService.java
+++ src/main/java/com/handongapp/cms/service/CourseService.java
@@ -5,7 +5,7 @@ import com.handongapp.cms.dto.v1.CourseDto;
 public interface CourseService {
     CourseDto.Response create(String clubSlug, String userId, CourseDto.CreateRequest req);
     CourseDto.Response getBySlug(String slug);
-    CourseDto.Response updateBySlug(String clubSlug, String courslug, CourseDto.UpdateRequest req);
+    CourseDto.Response updateBySlug(String clubSlug, String courseSlug, CourseDto.UpdateRequest req);
     void deleteSoftBySlug(String slug);
     String getCourseDetailsAsJsonBySlug(String courseSlug);
 }
