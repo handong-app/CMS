c751096e6bf4bfda54dd3f8673167bcbc0237e77
callendev20 <callendev20@gmail.com>
2025-06-01 16:07:29 +0900

    :white_check_mark: be/feat: make DataUpdateException can handle cause

--- src/main/java/com/handongapp/cms/exception/data/DataUpdateException.java
+++ src/main/java/com/handongapp/cms/exception/data/DataUpdateException.java
@@ -16,4 +16,8 @@ public class DataUpdateException extends RuntimeException {
     public DataUpdateException(String message) {
         super(message);
     }
+
+    public DataUpdateException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
