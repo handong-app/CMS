957209095f49530e87beda967ce6ac0894153c5c
ejPark43 <pej22200310@handong.ac.kr>
2025-06-01 17:38:47 +0900

    🚨fix: 누락된 BE 파일 추가

---
+++ src/main/java/com/handongapp/cms/controller/v1/S3Controller.java
@@ -0,0 +1,43 @@
+package com.handongapp.cms.controller.v1;
+
+import com.handongapp.cms.dto.v1.S3Dto;
+import com.handongapp.cms.service.PresignedUrlService;
+import com.handongapp.cms.service.UploadNotifyService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/s3")
+@RequiredArgsConstructor
+public class S3Controller {
+
+    private final PresignedUrlService presignedUrlService;
+    private final UploadNotifyService uploadNotifyService;
+
+    @PostMapping("/upload-url")
+    public ResponseEntity<S3Dto.UploadUrlResponse> generateUploadUrl(
+            @RequestBody S3Dto.UploadUrlRequest request) {
+        return ResponseEntity.ok(
+                S3Dto.UploadUrlResponse.builder()
+                        .presignedUrl(presignedUrlService.generateUploadUrl(request.getFilename(), request.getContentType()).toString())
+                        .build()
+        );
+    }
+
+    @PostMapping("/upload-complete")
+    public ResponseEntity<Void> notifyUploadComplete(@RequestBody S3Dto.UploadCompleteDto dto) {
+        uploadNotifyService.notifyUploadComplete(dto);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/download-url")
+    public ResponseEntity<S3Dto.DownloadUrlResponse> generateDownloadUrl(
+            @RequestParam("filename") String filename) {
+        return ResponseEntity.ok(
+                S3Dto.DownloadUrlResponse.builder()
+                        .presignedUrl(presignedUrlService.generateDownloadUrl(filename).toString())
+                        .build()
+        );
+    }
+}
