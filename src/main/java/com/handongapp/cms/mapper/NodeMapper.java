d9316d7f9429724aa4e68cb01a15be80b8d8a80e
callendev20 <callendev20@gmail.com>
2025-06-01 16:26:33 +0900

    :sparkles: be/feat: implement NodeMapper.updateNodeData

--- src/main/java/com/handongapp/cms/mapper/NodeMapper.java
+++ src/main/java/com/handongapp/cms/mapper/NodeMapper.java
@@ -8,9 +8,14 @@ public interface NodeMapper {
      * 노드 ID로 클럽ID를 조회
      */
     String findClubIdByNodeId(@Param("nodeId") String nodeId);
+
     /**
      * 노드 ID로 타입을 조회
      */
     TbNode.NodeType findNodeTypeById(@Param("nodeId") String nodeId);
 
+    /**
+     * 노드 ID로 data를 갱신
+     */
+    void updateNodeData(@Param("nodeId") String nodeId, @Param("data") String dataJson);
 }
