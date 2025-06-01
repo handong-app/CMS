a4ecf9bb84caafab46e7d929b8bb18163ed97b9e
callendev20 <callendev20@gmail.com>
2025-06-01 16:27:43 +0900

    :white_check_mark: be/feat: add findByFileKey and findByFileKeyStartingWith to FileListRepository

--- src/main/java/com/handongapp/cms/repository/FileListRepository.java
+++ src/main/java/com/handongapp/cms/repository/FileListRepository.java
@@ -4,7 +4,12 @@ import com.handongapp.cms.domain.TbFileList;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 
 @Repository
 public interface FileListRepository extends JpaRepository<TbFileList, String> {
+    Optional<TbFileList> findByFileKey(String fileKey);
+    List<TbFileList> findByFileKeyStartingWith(String prefix);
 }
