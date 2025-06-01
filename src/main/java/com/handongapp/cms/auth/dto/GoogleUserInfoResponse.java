5933680574a2d59929115a4e0ef333829776b4b2
Jungsub Ryoo <junglesubmarine@gmail.com>
2025-06-02 02:37:27 +0900

    🚑️ be/hotfix: add no-args constructor to enable Jackson deserialization of GoogleUserInfoResponse (#118)

--- src/main/java/com/handongapp/cms/auth/dto/GoogleUserInfoResponse.java
+++ src/main/java/com/handongapp/cms/auth/dto/GoogleUserInfoResponse.java
@@ -1,11 +1,15 @@
 package com.handongapp.cms.auth.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 public class GoogleUserInfoResponse {
@@ -15,5 +19,4 @@ public class GoogleUserInfoResponse {
     @JsonProperty("given_name")  private String givenName;
     @JsonProperty("family_name")  private String familyName;
     private String picture;
-    private String locale;
 }
