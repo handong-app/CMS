daaa0bca75215776463fc74be40f3eca9e11ae23
callendev20 <callendev20@gmail.com>
2025-06-01 18:40:35 +0900

    :fire: be/chore: remove unused import from PresignedUrlService

--- src/main/java/com/handongapp/cms/service/PresignedUrlService.java
+++ src/main/java/com/handongapp/cms/service/PresignedUrlService.java
@@ -2,8 +2,6 @@ package com.handongapp.cms.service;
 
 import com.handongapp.cms.dto.v1.S3Dto;
 
-import java.net.URL;
-
 
 public interface PresignedUrlService {
     S3Dto.UploadUrlResponse generateNodeFileUploadUrl(S3Dto.NodeFileUploadUrlRequest request, String userId);
