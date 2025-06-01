0d223a2b6eeb29848c912ca7124699ad93140a76
callendev20 <callendev20@gmail.com>
2025-06-01 18:13:04 +0900

    :white_check_mark: be/feat: add findByNodeId and remove unused method - FileListRepository

--- src/main/java/com/handongapp/cms/repository/FileListRepository.java
+++ src/main/java/com/handongapp/cms/repository/FileListRepository.java
@@ -5,11 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
-
 
 @Repository
 public interface FileListRepository extends JpaRepository<TbFileList, String> {
-    Optional<TbFileList> findByFileKey(String fileKey);
-    List<TbFileList> findByFileKeyStartingWith(String prefix);
+    List<TbFileList> findByNodeId(String nodeId);
 }
