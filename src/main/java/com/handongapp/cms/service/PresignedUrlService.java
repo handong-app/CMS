36cc0f8f86658a9c2a530894eb0bebb557e2ffd3
callendev20 <callendev20@gmail.com>
2025-06-02 20:56:27 +0900

    :recycle: be/refactor: add duration arg to PresignedUrlService.generateDownloadUrl

--- src/main/java/com/handongapp/cms/service/PresignedUrlService.java
+++ src/main/java/com/handongapp/cms/service/PresignedUrlService.java
@@ -2,7 +2,11 @@ package com.handongapp.cms.service;
 
 import com.handongapp.cms.dto.v1.S3Dto;
 
+import java.net.URL;
+import java.time.Duration;
+
 
 public interface PresignedUrlService {
     S3Dto.UploadUrlResponse generateNodeFileUploadUrl(S3Dto.NodeFileUploadUrlRequest request, String userId);
+    URL generateDownloadUrl(String key, Duration duration);
 }
